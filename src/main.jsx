2fbc6b3a74f5076fcf57feef76173b8a9c64fa6b
Santript Mehta <santriptmehta@gmail.com>
2025-04-17 02:19:54 +0530

    initial commit 2

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+import NavBar from './component/navbar.jsx'
+import HeadComponent from './component/headComponent.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <NavBar/>
+    <HeadComponent/>
   </StrictMode>,
 )
