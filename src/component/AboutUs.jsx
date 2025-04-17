0e4da89f3748a0deead909794754d451e638fd16
Santript Mehta <santriptmehta@gmail.com>
2025-04-17 23:26:43 +0530

    initial commit 4

--- src/component/AboutUs.jsx
+++ src/component/AboutUs.jsx
@@ -1,16 +1,14 @@
 import "./style/navbar.css";
 
 const links = [
-  { name: "Open roles", href: "#" },
-  { name: "Internship program", href: "#" },
   { name: "Our values", href: "#" },
   { name: "Meet our leadership", href: "#" },
 ];
 const stats = [
-  { name: "Offices worldwide", value: "12" },
-  { name: "Full-time colleagues", value: "300+" },
-  { name: "Hours per week", value: "40" },
-  { name: "Paid time off", value: "Unlimited" },
+  { name: "Served Clients", value: "-+" },
+  { name: "Total Products", value: "400+" },
+  { name: "Served Region", value: "+-" },
+  { name: "Availablity", value: "+-" },
 ];
 
 export default function AboutUs() {
